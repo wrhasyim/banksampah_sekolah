d8041c3d18768629364bbff06213cd09e0734b36
wrhasyim <wrhasyim98@gmail.com>
2025-08-26 08:39:26 +0700

    Import Siswa

--- database/seeders/MasterDataSeeder.php
+++ database/seeders/MasterDataSeeder.php
@@ -18,14 +18,37 @@ class MasterDataSeeder extends Seeder
         Schema::enableForeignKeyConstraints();
 
         Kelas::insert([
+            ['nama_kelas' => 'Kelas X MPLB 1', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X MPLB 2', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X TM 1', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X TM 2', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X TM 3', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X TM 4', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X TM 5', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas X TM 6', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI MPLB 1', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI MPLB 2', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 1', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 2', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 3', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 4', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 5', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 6', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XI TM 7', 'created_at' => now(), 'updated_at' => now()],
             ['nama_kelas' => 'Kelas XII MPLB 1', 'created_at' => now(), 'updated_at' => now()],
             ['nama_kelas' => 'Kelas XII MPLB 2', 'created_at' => now(), 'updated_at' => now()],
             ['nama_kelas' => 'Kelas XII TM 1', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XII TM 2', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XII TM 3', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XII TM 4', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XII TM 5', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XII TM 6', 'created_at' => now(), 'updated_at' => now()],
+            ['nama_kelas' => 'Kelas XII TM 7', 'created_at' => now(), 'updated_at' => now()],
         ]);
 
         JenisSampah::insert([
-            ['nama_sampah' => 'Botol Plastik', 'harga_per_satuan' => 500, 'stok' => 0],
-            ['nama_sampah' => 'Gelas Plastik', 'harga_per_satuan' => 1000, 'stok' => 0],
+            ['nama_sampah' => 'Botol Plastik', 'harga_per_satuan' => 35, 'stok' => 0],
+            ['nama_sampah' => 'Gelas Plastik', 'harga_per_satuan' => 20, 'stok' => 0],
         ]);
     }
 }
