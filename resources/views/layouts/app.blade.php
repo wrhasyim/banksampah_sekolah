9e5dd3ea5a789f36bd9f4c12a067977994223d83
wrhasyim <wrhasyim98@gmail.com>
2025-08-31 00:44:40 +0700

    remove header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-g">
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -16,16 +16,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            {{-- BAGIAN HEADER DIHAPUS DARI SINI --}}
 
             <main>
                 {{ $slot }}
@@ -39,13 +33,10 @@
 
         {{-- Skrip untuk Menampilkan Notifikasi dari Session --}}
         <script>
-            // Atur posisi notifikasi
             toastr.options.positionClass = 'toast-bottom-right';
-
             @if(session('toastr-success'))
                 toastr.success("{{ session('toastr-success') }}");
             @endif
-
             @if(session('toastr-error'))
                 toastr.error("{{ session('toastr-error') }}");
             @endif
