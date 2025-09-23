11749b3977b492028b5b43e5e0080cd5efeb3767
wrhasyim <wrhasyim98@gmail.com>
2025-09-23 09:13:58 +0700

    fix: Update generation time in SQL dump

--- database/db_banksampah.sql
+++ database/db_banksampah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 23, 2025 at 04:12 AM
+-- Generation Time: Sep 23, 2025 at 04:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
