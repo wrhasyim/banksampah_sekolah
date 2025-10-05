06427e93514ce61e753ec48a904169c424c67033
wrhasyim <wrhasyim98@gmail.com>
2025-10-06 03:54:08 +0700

    fix: Correctly include tom-select CSS and JS files for proper functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+<link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.bootstrap5.css" rel="stylesheet">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         {{-- CSS untuk Toastr (Notifikasi) --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
@@ -74,7 +74,7 @@
             });
         }
     </script>
-
+<script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
     @stack('scripts')
         
     </body>
